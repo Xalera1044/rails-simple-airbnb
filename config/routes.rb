b6cd188396de452498f0282982ff2061e853a16a
Abbas Nazarov <abbas.nazarov@gmail.com>
2022-11-16 18:25:17 +0400

    seed done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'flats/index'
+  get 'flats/show'
+  get 'flats/edit'
+  get 'flats/update'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Flat.create!(
+  name: 'nice',
+  address: '15 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 105,
+  number_of_guests: 1
+)
+
+Flat.create!(
+  name: 'Light & Span',
+  address: '11 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 7500,
+  number_of_guests: 30
+)
+
+Flat.create!(
+  name: 'Summer',
+  address: '7 sallakhana 7',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 705,
+  number_of_guests: 4
+)
