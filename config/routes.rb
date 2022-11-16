0e09ed75b38afe6cd01d8708fb6df0c0905fc0fa
Abbas Nazarov <abbas.nazarov@gmail.com>
2022-11-16 19:10:02 +0400

    bootstarp

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'flats/index'
-  get 'flats/show'
-  get 'flats/edit'
-  get 'flats/update'
+  # get 'flats/index'
+  # get 'flats/show'
+  # get 'flats/edit'
+  # get 'flats/update'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :flats
 end
