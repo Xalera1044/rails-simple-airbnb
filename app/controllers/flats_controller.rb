16f258b8d5315b39a5042bfb24f0a5f250d4269c
Abbas Nazarov <abbas.nazarov@gmail.com>
2022-11-16 21:12:06 +0400

    bootstarp

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,14 +1,49 @@
 class FlatsController < ApplicationController
   def index
-    @flats = Flat.all
+    if params[:query].present?
+      @query = params[:query]
+      @flats = Flat.where("name LIKE ?", "%#{params[:query]}%")
+      # Preventing SQL Injection and Database error for
+      # unknown characters
+    else
+      @flats = Flat.all
+    end
   end
 
   def show
+  end
+
+  def new
+    @flats = Flat.new
   end
 
   def edit
   end
 
   def update
+    if @flat.update(flat_params)
+      redirect_to flat_path(@flat)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flats_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests, :picture_url)
   end
 end
